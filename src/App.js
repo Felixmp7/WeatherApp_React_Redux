2125d680075272eccc1ed603e336e16917db5ae3
Felix Pacheco <devfex.19@gmail.com>
2020-08-07 20:50:46 -0400

    cleaning

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import Home from './pages/home/weatherLocation';
-// import logo from './logo.svg';
-// import './App.css';
 
-const App = () => (
-  <Home/>
-)
+const App = () => <Home/>;
 
 export default App;
