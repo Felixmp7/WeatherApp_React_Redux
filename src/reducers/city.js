28987350a09c6144ed66393d5735f827801d7a03
Felix Pacheco <devfex.19@gmail.com>
2020-08-15 13:56:49 -0400

    documenting

--- src/reducers/city.js
+++ src/reducers/city.js
@@ -1,9 +1,16 @@
 import { SELECT_CITY } from '../actions/index'
 
+// Funciones Puras
+/* 
+  Consisten en funciones que solo trabajan y realizan operaciones con los
+  parametros que le pasamos, no utiliza otras variables, ni depende
+  de otros datos, sólo de los que recibe por parametros.
+*/
+
 export const setFirstWeather = (state, action) => {
   switch (action.type) {
     case SELECT_CITY:
-      return { ...state, citySelected: action.value}
+      return { ...state, citySelected: action.payload};
       break;
 
     default: return state;
